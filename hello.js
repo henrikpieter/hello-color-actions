d26d7254dfdb11f7f49d63ea76559d702cc1e83c
henrikpieter <henrikpieter@users.noreply.github.com>
2024-01-16 17:15:32 +0100

    Boodschap gewijzigd

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from a cold Utrecht, The Netherlands!";
 console.log(hello());
 exports.hello = hello;
